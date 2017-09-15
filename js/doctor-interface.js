a8e9eedd78d54941770c0583343ac19eabd82f9d
Rich Garrick <richg341@gmail.com>
2017-09-15 14:42:09 -0700

    display doctor info to front end interface

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,18 +1,20 @@
 import { doctor } from "./../js/doctor.js";
 
 $(document).ready(function(){
-// function displayDoctors(){
-//   doctorArray.forEach(function(doctor){
-//     $('#doctorInfo').append('<p>' + bike.title);
-//   });
-// }
+function displayDoctors(callDoctor){
+  console.log("in front end function");
+  console.log(callDoctor);
+  callDoctor.forEach(function(test){
+    $('#doctorInfo').append(`<p>${test.fName} ${test.mName} ${test.lName}, ${test.title} `);
+
+  });
+}
 
 $('#userInputQuery').submit(function(e){
   e.preventDefault();
   let query = $("input[name='userInputQuery']").val();
   $("input").val("");
-  console.log(query);
-  doctor.findDoctorByQuery(query);
+  doctor.findDoctorByQuery(query, displayDoctors);
 });
 
 
--- js/doctor.js
+++ js/doctor.js
@@ -3,16 +3,37 @@ var apiKey = require('./../.env').apiKey;
 export let doctor = {
 
 
-  // parseApiReturn: function(apiRequest1){
-  //
-  // }
+parseApiReturn: function(test, displayDoctors){
+  let callDoctor = [];
+  test.data.forEach(function(test){
+    callDoctor.push(
+      {
+        fName: test.profile.first_name,
+        mName: test.profile.middle_name,
+        lName: test.profile.last_name,
+        title: test.profile.title,
+        image_url: test.profile.image_url,
+        // street: test.practices.visit_address.street,
+        // city: test.practices.city,
+        // state: test.practices.state,
+        // zip: test.practices.zip,
+        // lat: test.practices.lat,
+        // lng:test.practices.lon,
+        // // phone: test.practices.phones.number,
+        // website: test.practices.website,
+        // acceptingPatients: test.practices.accepts_new_patients,
+      }
+    );
+  });
+  displayDoctors(callDoctor);
+},
 
-findDoctorByQuery: function(query) {
-  console.log("function called");
-  let apiRequest1 = fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=${apiKey}&limit=1&query=${query}`)
+
+findDoctorByQuery: function(query, displayDoctors) {
+  let apiRequest1 = fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=${apiKey}&limit=2&query=${query}`)
   .then((res) => {
-    res.json().then((data) => {
-      console.log(data);
+    res.json().then((test) => {
+      doctor.parseApiReturn(test, displayDoctors);
     });
   })
   .catch((error) => {
