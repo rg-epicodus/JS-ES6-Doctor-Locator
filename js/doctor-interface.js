ff2ee6cb49ede1207d0e22fa53a8330b652f4b3b
Rich Garrick <richg341@gmail.com>
2017-09-15 11:59:59 -0700

    add function to call api from backend

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,10 +1,18 @@
-import { doctor } from "./../js/doctor.js"
+import { doctor } from "./../js/doctor.js";
 
 $(document).ready(function(){
+// function displayDoctors(){
+//   doctorArray.forEach(function(doctor){
+//     $('#doctorInfo').append('<p>' + bike.title);
+//   });
+// }
+
 $('#userInputQuery').submit(function(e){
   e.preventDefault();
   let query = $("input[name='userInputQuery']").val();
   $("input").val("");
+  console.log(query);
+  doctor.findDoctorByQuery(query);
 });
 
 
--- js/doctor.js
+++ js/doctor.js
@@ -1,20 +1,33 @@
-// user enters medial issue and returns list of doctors in portland area that fit query
-// user enters name to retrieve list of doctors in portland area that fit query
-
-// docotrs returned need to show: First name, last name, address, phone, website, accepting new patients status
-// api request errors (not 200 level) return error message
-// if no matches to query, notification to user
 var apiKey = require('./../.env').apiKey;
 
-// export let doctor {
-  let apiRequest1 = fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=${apiKey}&limit=1&query=diabetes`)
+export let doctor = {
+
+
+  // parseApiReturn: function(apiRequest1){
+  //
+  // }
+
+findDoctorByQuery: function(query) {
+  console.log("function called");
+  let apiRequest1 = fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=${apiKey}&limit=1&query=${query}`)
   .then((res) => {
     res.json().then((data) => {
       console.log(data);
     });
   })
+  .catch((error) => {
+    console.log(error);
+  });
 
-// }
+
+}
+
+};
+
+
+
+
+
 
 
 // let apiRequest2 = fetch('http://api.openweathermap.org/data/2.5/forecast?q=seattle&appid=da2fec2009d72c8dd21ed03e0419a232')
