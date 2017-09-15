b01ac87773f8c342176a14dc1be6ccbbb32a0da0
Rich Garrick <richg341@gmail.com>
2017-09-15 10:28:07 -0700

    add first working api call

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,13 @@
-// import { doctor } from "./../js/doctor.js"
-//
-// $(document).ready(function(){
-// $('#userInputQuery').submit(function(e){
-//   e.preventDefault();
-//   let query = $("input[name='userInputQuery']").val();
-//   $("input").val("");
-// });
-//
-//
-//
-//
-// });
+import { doctor } from "./../js/doctor.js"
+
+$(document).ready(function(){
+$('#userInputQuery').submit(function(e){
+  e.preventDefault();
+  let query = $("input[name='userInputQuery']").val();
+  $("input").val("");
+});
+
+
+
+
+});
--- js/doctor.js
+++ js/doctor.js
@@ -4,16 +4,16 @@
 // docotrs returned need to show: First name, last name, address, phone, website, accepting new patients status
 // api request errors (not 200 level) return error message
 // if no matches to query, notification to user
-// var apiKey = require('./../.env').apiKey;
-//
+var apiKey = require('./../.env').apiKey;
+
 // export let doctor {
-//   let apiRequest1 = fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=${apiKey}&limit=1&query=diabetes`)
-//   .then((res) => {
-//     res.json().then((data) => {
-//       console.log(data);
-//     });
-//   })
-//
+  let apiRequest1 = fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=${apiKey}&limit=1&query=diabetes`)
+  .then((res) => {
+    res.json().then((data) => {
+      console.log(data);
+    });
+  })
+
 // }
 
 
