a15bb4d31bdb089db2bea3b6c04dc85434ea3588
Rich Garrick <richg341@gmail.com>
2017-09-15 10:15:37 -0700

    install dependencies to project

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,13 @@
-var apiKey = require('./../.env').apiKey;
+// import { doctor } from "./../js/doctor.js"
+//
+// $(document).ready(function(){
+// $('#userInputQuery').submit(function(e){
+//   e.preventDefault();
+//   let query = $("input[name='userInputQuery']").val();
+//   $("input").val("");
+// });
+//
+//
+//
+//
+// });
---
+++ js/doctor.js
@@ -0,0 +1,32 @@
+// user enters medial issue and returns list of doctors in portland area that fit query
+// user enters name to retrieve list of doctors in portland area that fit query
+
+// docotrs returned need to show: First name, last name, address, phone, website, accepting new patients status
+// api request errors (not 200 level) return error message
+// if no matches to query, notification to user
+// var apiKey = require('./../.env').apiKey;
+//
+// export let doctor {
+//   let apiRequest1 = fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=${apiKey}&limit=1&query=diabetes`)
+//   .then((res) => {
+//     res.json().then((data) => {
+//       console.log(data);
+//     });
+//   })
+//
+// }
+
+
+// let apiRequest2 = fetch('http://api.openweathermap.org/data/2.5/forecast?q=seattle&appid=da2fec2009d72c8dd21ed03e0419a232')
+//   .then((res) => {
+//     res.json().then((data) => {
+//       console.log(data);
+//     });
+//   })
+//
+// Promise.all([apiRequest1, apiRequest2])
+//   .then((data) => {
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
